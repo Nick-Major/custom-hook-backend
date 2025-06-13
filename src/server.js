37d6ec477f5af20b52454bb8f53eccba0a656664
Nick-Major <divanchik26@gmail.com>
2025-06-13 19:48:25 +0300

    2d commit

--- src/server.js
+++ src/server.js
@@ -4,33 +4,31 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors());
-app.use(
-  bodyParser.json({
-    type(req) {
-      return true;
-    },
-  })
-);
-app.use(function (req, res, next) {
-  res.setHeader("Content-Type", "application/json");
-  next();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS']
+}));
+app.use(bodyParser.json());
+
+app.get("/data", (req, res) => {
+  res.json({ status: "ok" });
 });
 
-app.get("/data", async (req, res) => {
-  res.send(JSON.stringify({ status: "ok" }));
+app.get("/error", (req, res) => {
+  res.status(500).json({ status: "Internal Error" });
 });
-app.get("/error", async (req, res) => {
-  res.status(500).send(JSON.stringify({ status: "Internal Error" }));
-});
+
 app.get("/loading", async (req, res) => {
   await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 5000);
+    setTimeout(resolve, 5000);
   });
-  res.send(JSON.stringify({ status: "ok" }));
+  res.json({ status: "ok" });
 });
 
-const port = process.env.PORT || 7070;
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`The server is running on port ${port}.`));
